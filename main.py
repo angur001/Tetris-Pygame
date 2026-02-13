7abab1494d42f4b79945f3c493c3cbaf02b093ee
angur001 <70037460+angur001@users.noreply.github.com>
2026-02-13 01:02:36 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Tetris Test")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((50, 50, 200))
+    pygame.display.flip()
+
+pygame.quit()
