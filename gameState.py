30ae50e11b6937468ff392139686babdaaab2ede
angur001 <70037460+angur001@users.noreply.github.com>
2026-02-16 01:47:37 +0100

    better naming

--- gameState.py
+++ gameState.py
@@ -41,21 +41,21 @@ class GameState:
 
     def step(self):
         if self.currentShape:
-            if not self.isAtBottom():
+            if self.canMoveDown():
                 self.removeShadow()
                 self.currentShape.y += 1
                 self.updateGameGrid()
             else:
                 self.addRandomShape()
     
-    def isAtBottom(self):
+    def canMoveDown(self):
         for j in range(len(self.currentShape.matrix[0])):
             for i in range(len(self.currentShape.matrix) - 1, -1, -1):
                 if self.currentShape.matrix[i][j] != Cell.Empty:
                     if self.currentShape.y + i + 1 >= self.HEIGHT or self.gameGrid[self.currentShape.y + i + 1][self.currentShape.x + j] != Cell.Empty:
-                        return True
+                        return False
                     break
-        return False
+        return True
 
     def canMoveLeft(self):
         for i in range(len(self.currentShape.matrix)):
